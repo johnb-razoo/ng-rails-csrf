e0cbc12b86fcbe838dbb7e5d3d29514c0e83dba6
johnb-razoo <john@razoo.com>
2013-05-01 11:46:40 -0700

    Initial fix for Rails2 (untested)

--- vendor/assets/javascripts/ng-rails-csrf.js
+++ vendor/assets/javascripts/ng-rails-csrf.js
@@ -1,7 +1,10 @@
 angular.module('ng-rails-csrf', [] ).config(['$httpProvider', function($httpProvider) {    
     var getToken = function() { 
-        var el = document.querySelector('meta[name="csrf-token"]');
-        return el && el.getAttribute('content');
+        // Rails 3+
+        //var el = document.querySelector('meta[name="csrf-token"]');
+        //return el && el.getAttribute('content');
+        // Rails 2
+        return document.querySelector('input[name="authenticity_token"]').value;
     };
     var updateToken = function() {
         var headers = $httpProvider.defaults.headers.common, token = getToken();
